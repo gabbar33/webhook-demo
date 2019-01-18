ca58e20b6f7a8fde7d87e198a06394d629c4d370
gabbarsisodiya <gabbar.sisodiya@nagarro.com>
2019-01-18 16:31:06 +0530

    add log into post request

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+      log.info(webhook_event);
     });
 
     // Returns a '200 OK' response to all requests
